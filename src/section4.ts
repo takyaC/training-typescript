60f599d2ab8c47f72011fb0fa1e8b730f9926047
Takashi Watanabe <t-watanabe@iret.co.jp>
2020-10-18 01:19:12 +0900

    fix

--- src/section4.ts
+++ src/section4.ts
@@ -6,30 +6,34 @@
 // 4-1. 無い場合はunknown
 (function () {
 
-  // // function getFoo(obj) {
-  // //   return obj.foo;
-  // // }
-  // function getFoo<T extends { foo: V }, V>(obj:T): V {
+  // 不正解!!!
+  // function getFoo<T extends { foo: V } , V>(obj:T): V{
   //   return obj.foo;
   // }
 
-  // // 使用例
-  // // numはnumber型
-  // const num = getFoo({
-  //   foo: 123
-  // });
-  // // strはstring型
-  // const str = getFoo({
-  //   foo: "hoge",
-  //   bar: 0
-  // });
-  // // unkはunknown型
-  // const unk = getFoo({
-  //   hoge: true
-  // });
+  // 正解!!
+  // TODO: infer? emit?
+  function getFoo<T extends object>(obj: T): T extends { foo: infer E } ? E : unknown {
+    return (obj as any).foo;
+  }
 
-  // // エラー例
-  // getFoo(123);
-  // getFoo(null);
+  // 使用例
+  // numはnumber型
+  const num = getFoo({
+    foo: 123
+  });
+  // strはstring型
+  const str = getFoo({
+    foo: "hoge",
+    bar: 0
+  });
+  // unkはunknown型
+  const unk = getFoo({
+    hoge: true
+  });
+
+  // エラー例
+  getFoo(123);
+  getFoo(null);
 
 })();
