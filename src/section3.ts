7f6bdcda820fa43a25ed54ebaeed1ef76d357268
Takashi Watanabe <t-watanabe@iret.co.jp>
2020-10-18 00:17:34 +0900

    fix

--- src/section3.ts
+++ src/section3.ts
@@ -182,26 +182,28 @@
   // });
 })();
 
-
-// TODO: NG
 // 3-5. undefinedな引数
 (function () {
+
+  // 不正解!!
   // type Func<A, R> = (arg?: A) => R;
-  // type Func<A, R, B=undefined> = (arg: A | B) => R;
 
-  // // 使用例
-  // const f1: Func<number, number> = num => num + 10;
-  // const v1: number = f1(10);
+  // 正解!!
+  type Func<A, R> = undefined extends A ? (arg?: A) => R : (arg: A) => R;
 
-  // const f2: Func<undefined, number> = () => 0;
-  // const v2: number = f2();
-  // const v3: number = f2(undefined);
+  // 使用例
+  const f1: Func<number, number> = num => num + 10;
+  const v1: number = f1(10);
 
-  // const f3: Func<number | undefined, number> = num => (num || 0) + 10;
-  // const v4: number = f3(123);
-  // const v5: number = f3();
+  const f2: Func<undefined, number> = () => 0;
+  const v2: number = f2();
+  const v3: number = f2(undefined);
 
-  // // エラー例
+  const f3: Func<number | undefined, number> = num => (num || 0) + 10;
+  const v4: number = f3(123);
+  const v5: number = f3();
+
+  // エラー例
   // const v6: number = f1();
 
 })();
